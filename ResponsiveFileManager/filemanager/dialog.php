5e3074c0758e808662d822e803a5c87a7da1008e
Matt Gordon <gordon_matt@hotmail.com>
2025-05-19 09:55:49 +0930

    A few more fixes

--- ResponsiveFileManager/filemanager/dialog.php
+++ ResponsiveFileManager/filemanager/dialog.php
@@ -112,19 +112,17 @@ if ($config['show_total_size']) {
 // Initialize $rfm_subfolder
 $rfm_subfolder = '';
 
-// Handle rootFolder (tenant/user folder) - only set if explicitly provided and different
+// Handle rootFolder (tenant/user folder) - only set if explicitly provided
 if (isset($_GET['rootFolder']) && !empty($_GET['rootFolder'])) {
     $new_root = trim($_GET['rootFolder'], '/');
-    if (!isset($_SESSION['RF']["subfolder"]) || $_SESSION['RF']["subfolder"] !== $new_root) {
+    if (!isset($_SESSION['RF']["subfolder"])) {
         $_SESSION['RF']["subfolder"] = $new_root;
     }
-    $rfm_subfolder = $_SESSION['RF']["subfolder"];
 } else if (!isset($_SESSION['RF']["subfolder"])) {
     $_SESSION['RF']["subfolder"] = '';
-    $rfm_subfolder = '';
-} else {
-    $rfm_subfolder = $_SESSION['RF']["subfolder"];
 }
+
+$rfm_subfolder = '';
 
 // Handle subfolder (nested folder)
 if (!empty($_SESSION['RF']["subfolder"])
@@ -423,12 +421,18 @@ $get_params = http_build_query($get_params);
     echo $version; ?>"></script>
 </head>
 <body>
+<!-- Change: 2022-02-22: See https://github.com/trippo/ResponsiveFilemanager/issues/608 -->
+<script src="js/blueimp/tmpl.min.js"></script>
+<script src="js/blueimp/load-image.all.min.js"></script>
+<script src="js/blueimp/canvas-to-blob.min.js"></script>
+
 <!-- The Templates plugin is included to render the upload/download listings -->
-<script src="//blueimp.github.io/JavaScript-Templates/js/tmpl.min.js"></script>
+<!--<script src="//blueimp.github.io/JavaScript-Templates/js/tmpl.min.js"></script> -->
 <!-- The Load Image plugin is included for the preview images and image resizing functionality -->
-<script src="//cdnjs.cloudflare.com/ajax/libs/blueimp-load-image/2.18.0/load-image.all.min.js"></script>
+<!--<script src="//cdnjs.cloudflare.com/ajax/libs/blueimp-load-image/2.18.0/load-image.all.min.js"></script> -->
 <!-- The Canvas to Blob plugin is included for image resizing functionality -->
-<script src="//blueimp.github.io/JavaScript-Canvas-to-Blob/js/canvas-to-blob.min.js"></script>
+<!--<script src="//blueimp.github.io/JavaScript-Canvas-to-Blob/js/canvas-to-blob.min.js"></script> -->
+
 <!-- The Iframe Transport is required for browsers without support for XHR file uploads -->
 <script src="js/jquery.iframe-transport.js"></script>
 <!-- The basic File Upload plugin -->
